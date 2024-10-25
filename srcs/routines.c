69b5d75f3f3a65ae627ac97a6ff8308c00bc1990
NeverBackPing <neverbackping@gmail.com>
2024-10-25 15:36:10 +0200

    start rout pair

--- srcs/routines.c
+++ srcs/routines.c
@@ -13,20 +13,28 @@
 #include "../includes/philosophers.h"
 
 
-void	philo_pair(t_philo *philo)
+bool	philo_pair(t_philo *philo)
 {
-	while (1)
+	while (true)
 	{
 		pthread_mutex_lock(&philo->data->write);
 		pthread_mutex_lock(&philo->fork);
 		if (philo->id % 2 == 0)
 		{
-			printf("philo %d : I eat \n", philo->id);
+			printf("philo %d: 🍜\n", philo->id);
 			ft_usleep(philo->pars->time_eat);
 		}
 		pthread_mutex_unlock(&philo->fork);
 		pthread_mutex_unlock(&philo->data->write);
+		printf("philo %d : I sleep 😴\n", philo->id);
+		ft_usleep(philo->pars->time_sleep);
 	}
+	return (true);
+}
+
+bool	philo_odd(t_philo *philo)
+{
+	return (true);
 }
 
 void	*routine(void *args)
@@ -34,20 +42,12 @@ void	*routine(void *args)
 	t_philo	*philo;
 
 	philo = (t_philo *)args;
-	/*pthread_mutex_lock(&philo->fork);
-	pthread_mutex_unlock(&philo->fork);*/
 	while (true)
 	{
-		if (philo->pars->nb_philo % 2 == 0)
-		{
-			//printf("philo %d: 🍜\n", philo->id);
-
-		}
-		else
-		{
-			//printf("philo %d : I sleep 😴\n", philo->id);
-			ft_usleep(philo->pars->time_sleep);
-		}
+		if (philo->pars->nb_philo % 2 == 0 && !philo_pair(t_philo *philo))
+			break ;
+		else if (philo->pars->nb_philo % 2 == 3 && !philo_odd(t_philo *philo))
+			break ;
 	}
 	return (SUCCESS);
 }
